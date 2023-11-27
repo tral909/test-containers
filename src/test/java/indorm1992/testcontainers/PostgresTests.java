78ea89205246d333191e4544c60bff2b3b1c2ad5
tral909 <indorm1992@mail.ru>
2023-11-27 15:19:29 +0300

    Prepare integration test container settings

--- src/test/java/indorm1992/testcontainers/PostgresTests.java
+++ src/test/java/indorm1992/testcontainers/PostgresTests.java
@@ -1,12 +1,44 @@
 package indorm1992.testcontainers;
 
+import indorm1992.testcontainers.repository.PostRepository;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @TestConfiguration(proxyBeanMethods = false)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PostgresTests {
+
+    @LocalServerPort
+    private Integer port;
+
+    @Container
+    @ServiceConnection
+    private static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:15.1-alpine"
+    );
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost:" + port;
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void test() {
+
+    }
 
 }
